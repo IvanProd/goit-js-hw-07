80891ac0c6743fbc8c464592c4ddf3e2bf977706
Ivan Prodeus <learning.webpractice@gmail.com>
2022-08-13 22:15:21 +0300

    update files from terminal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const divGallery = document.querySelector('.gallery');//отримали посилання на div галереї
+//console.log(divGallery);
+
+
+const createGalleryItem = (element) => {
+    return element.map(item => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </div>`
+    });
+};
+
+function makeGallery(massive){
+    const imageEl = createGalleryItem(massive);
+    return divGallery.insertAdjacentHTML('afterbegin', imageEl.join('')
+    );
+}
+
+makeGallery(galleryItems);
+
+
+
+
